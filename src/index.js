3b65b165ccb7467748f5451794d0a1156987622c
Bee Clark <mrhumanman2@gmail.com>
2021-11-26 14:17:41 -0500

    chore: Import Route and Routes

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@
 
 import React from "react";
 import ReactDom from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./app";
 import Home from "./routes/home";
 import "./index.css";
