2fdafae6721c60ba218c4e0979582233146970a9
Bee Clark <mrhumanman2@gmail.com>
2021-11-28 18:36:00 -0500

    style: Format files with prettier

--- src/app.js
+++ src/app.js
@@ -1,38 +1,50 @@
 /**
-  * Copyright 2021 Bee Clark
-  * This file is part of DetristaClient.
-  * 
-  * DetristaClient is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  * 
-  * DetristaClient is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-  * GNU General Public License for more details.
-  * 
-  * You should have received a copy of the GNU General Public License
-  * along with DetristaClient.  If not, see <https://www.gnu.org/licenses/>.
-  */
+ * Copyright 2021 Bee Clark
+ * This file is part of DetristaClient.
+ *
+ * DetristaClient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DetristaClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DetristaClient.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function App() {
-	const loggedIn = false;
+  const loggedIn = false;
 
-	return (
-		<div>
-			<nav>
-				<ul className="header">
-					<li><NavLink to="/">Home</NavLink></li>
-					<li><NavLink to="/tournament">Tournament</NavLink></li>
-					<li><NavLink to="/shop">Shop</NavLink></li>
-					<li><NavLink to="/community">Community</NavLink></li>
-					<li><NavLink to={loggedIn ? "/logout" : "/login"}>{loggedIn ? "Log Out" : "Log In"}</NavLink></li>
-				</ul>
-			</nav>
-			<Outlet />
-		</div>
-	)
+  return (
+    <div>
+      <nav>
+        <ul className="header">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tournament">Tournament</NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop">Shop</NavLink>
+          </li>
+          <li>
+            <NavLink to="/community">Community</NavLink>
+          </li>
+          <li>
+            <NavLink to={loggedIn ? "/logout" : "/login"}>
+              {loggedIn ? "Log Out" : "Log In"}
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 /**
-  * Copyright 2021 Bee Clark
-  * This file is part of DetristaClient.
-  * 
-  * DetristaClient is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  * 
-  * DetristaClient is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-  * GNU General Public License for more details.
-  * 
-  * You should have received a copy of the GNU General Public License
-  * along with DetristaClient.  If not, see <https://www.gnu.org/licenses/>.
-  */
+ * Copyright 2021 Bee Clark
+ * This file is part of DetristaClient.
+ *
+ * DetristaClient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DetristaClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DetristaClient.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import React from "react";
 import ReactDom from "react-dom";
@@ -24,12 +24,12 @@ import Home from "./routes/home";
 import "./styles/index.css";
 
 ReactDom.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />}>
-				<Route index element={<Home />} />
-			</Route>
-		</Routes>
-	</BrowserRouter>,
-	document.getElementById("root")
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
