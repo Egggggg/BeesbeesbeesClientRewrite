31720d03bda8068ee53093f3abe58be75b775104
Bee Clark <mrhumanman2@gmail.com>
2021-12-13 13:59:00 -0500

    feat: Wrap everything in Auth0Provider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -8,13 +9,18 @@ import Home from "./routes/home";
 import "./styles/root.css";
 
 ReactDom.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />}>
-				<Route index element={<Home />} />
-				<Route path="login" element={<Login />} />
-			</Route>
-		</Routes>
-	</BrowserRouter>,
+	<Auth0Provider
+		domain="dev-xaro0z-l.us.auth0.com"
+		clientId="QsDwvT3OyGqkm7NjXxlE0vZOajRPwk7Q"
+		redirectUri={window.location.origin}
+	>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</Auth0Provider>,
 	document.getElementById("root")
 );
