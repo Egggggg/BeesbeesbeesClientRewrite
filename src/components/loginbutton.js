f82a9dc13307c29a78708f846bbe96ed25444139
Bee Clark <mrhumanman2@gmail.com>
2021-12-13 13:59:34 -0500

    feat: Add LoginButton component

---
+++ src/components/loginbutton.js
@@ -0,0 +1,21 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function LoginButton() {
+	const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();
+
+	if (isLoading) {
+		return null;
+	}
+
+	return (
+		<a
+			onClick={() =>
+				isAuthenticated
+					? logout({ returnTo: window.location.origin })
+					: loginWithRedirect()
+			}
+		>
+			{isAuthenticated ? "Log Out" : "Log In"}
+		</a>
+	);
+}
