7b7a950cde2cecfbb89f24ba2c38bff7c278e3fe
Bee Clark <mrhumanman2@gmail.com>
2021-12-13 14:11:49 -0500

    feat: Return <a>Loading</a> instead of null

--- src/components/loginbutton.js
+++ src/components/loginbutton.js
@@ -4,7 +4,7 @@ export default function LoginButton() {
 	const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();
 
 	if (isLoading) {
-		return null;
+		return <a>Loading...</a>;
 	}
 
 	return (
