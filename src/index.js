eff1a511dad505f374f602074d1c83ac67ab6d92
Bee Clark <mrhumanman2@gmail.com>
2021-12-13 18:45:13 -0500

    refactor: Sort imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDom from "react-dom";
+
+import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./app";
+import config from "./config.json";
 import Home from "./routes/home";
+import Settings from "./routes/settings";
 
 import "./styles/root.css";
 
